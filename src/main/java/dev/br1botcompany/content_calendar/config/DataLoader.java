1f879b3bd821af1a439ad23dcd95aab61d2f1647
SturnioloBruno <brunosebastian.sturniolo@gmail.com>
2024-06-06 13:43:36 -0300

    insert data programmatically

---
+++ src/main/java/dev/br1botcompany/content_calendar/config/DataLoader.java
@@ -0,0 +1,36 @@
+package dev.br1botcompany.content_calendar.config;
+
+
+import dev.br1botcompany.content_calendar.model.Content;
+import dev.br1botcompany.content_calendar.model.Status;
+import dev.br1botcompany.content_calendar.model.Type;
+import dev.br1botcompany.content_calendar.repository.ContentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final ContentRepository repository;
+
+    public DataLoader(ContentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Content c = new Content(
+                null,
+                "Hello from a data loader",
+                "My first blog post",
+                Status.IDEA,
+                Type.VIDEO,
+                LocalDateTime.now(),
+                null,
+                ""
+        );
+        repository.save(c);
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS Content (
     PRIMARY KEY (id)
 );
 
-INSERT INTO Content(title,desc,status,content_type,date_created)
-VALUES ('My Spring Data Blog Post', 'A post about spring data', 'IDEA', 'ARTICLE', CURRENT_TIMESTAMP());
+--INSERT INTO Content(title,desc,status,content_type,date_created)
+--VALUES ('My Spring Data Blog Post', 'A post about spring data', 'IDEA', 'ARTICLE', CURRENT_TIMESTAMP());
