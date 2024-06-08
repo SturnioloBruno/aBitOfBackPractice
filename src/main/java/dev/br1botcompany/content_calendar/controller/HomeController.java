52b67cf847fce54528e376db64a9b4b557bbd163
SturnioloBruno <brunosebastian.sturniolo@gmail.com>
2024-06-08 10:51:10 -0300

    practicing @variables

---
+++ src/main/java/dev/br1botcompany/content_calendar/controller/HomeController.java
@@ -0,0 +1,22 @@
+package dev.br1botcompany.content_calendar.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @Value("${cc.welcomeMessage: Default Welcome Message}")
+    private String welcomeMessage;
+
+    @Value("${cc.about}")
+    private String about;
+
+    @GetMapping("/")
+    public Map<String, String> home() {
+        return Map.of("welcomeMessage", welcomeMessage, "about", about);
+    }
+}
